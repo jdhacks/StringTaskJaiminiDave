004fa499f2ad8d26a85e47f1b68ceec852aa74b5
jdhacks <62893934+jdhacks@users.noreply.github.com>
2021-07-10 10:23:48 +0530

    dynamic skip number

--- app/src/main/java/com/practical/stringtaskjaiminidave/MainActivity.java
+++ app/src/main/java/com/practical/stringtaskjaiminidave/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView txtinput,txtoutput;
     Button btnout;
-    EditText edtinput;
+    EditText edtinput,edtskip;
+    int skipwords=0;
     String giveninput="",inputprocess="",outtext="";
 
     @Override
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
            public void onClick(View view) {
                   outtext="";
                   giveninput="";
+                  skipwords=Integer.parseInt(edtskip.getText().toString().trim());
                giveninput= edtinput.getText().toString();
 
                System.out.println("pragraph ==="+giveninput);
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
                    String status="";
 
-                    if(inputprocess.split(" ").length>2) {
+                    if(inputprocess.split(" ").length>skipwords) {
                         status = reversesentance(inputprocess) + ".";
                     }else{
                         status=inputprocess+".";
@@ -71,11 +73,16 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("string ==="+s);
 
         String[] words = s.split(" ");
-            String name = words[words.length - 2] +" "+ words[words.length - 1] ;
+        //String name = words[words.length - 2] +" "+ words[words.length - 1] ;
+        String name="";
+        for(int i=1;i<=skipwords;i++)
+        {
+            name=name+" "+words[words.length-i];
+        }
         System.out.println("name===" + name);
 
         String last = "";
-            for (int i = 0; i <words.length-2 ; i++) {
+            for (int i = 0; i <words.length-skipwords ; i++) {
                 System.out.println("last===" + last);
 
                 last =  words[i]+" "+last;
@@ -94,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
     {
         edtinput=(EditText) findViewById(R.id.edtinput);
         txtoutput=(TextView) findViewById(R.id.txtonput);
-
+        edtskip= (EditText) findViewById(R.id.edtskipword);
         txtinput=(TextView) findViewById(R.id.lblInput);
         btnout=(Button) findViewById(R.id.btnoutput);
 
